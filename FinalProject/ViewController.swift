90a55f42b8f83244a07b6ec02d24203140d55dd4
Apple <apple@Apples-MacBook-Pro-86.local>
2019-06-26 12:02:19 -0400

    Initial commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Apple on 6/26/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction weak var destress: UIButton!
+    @IBAction weak var guidance: UIButton!
+    @IBAction weak var hotlines: UIButton!
+    
+    
+    
+    
+
+  override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
